7babea4367c03d9de143cf3a391708224d47d4b2
XandePedrassani <alexandrecouss@hotmail.com>
2025-03-04 10:07:23 -0300

    Aprendendo a usar a String.xml

--- app/src/main/java/com/pedrassani/primeirodemuitos/MainActivity.kt
+++ app/src/main/java/com/pedrassani/primeirodemuitos/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.pedrassani.primeirodemuitos.ui.theme.PrimeiroDeMuitosTheme
 
@@ -32,8 +33,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val helloDio = stringResource(id = R.string.hello_dio)
+
     Text(
-        text = "Hello $name! + ${som()}",
+        text = "$helloDio Dev-Alexandre",
         modifier = modifier
     )
 }
